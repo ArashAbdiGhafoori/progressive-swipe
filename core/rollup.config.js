3d37ff2e6524029df438e3b519559516c75ea287
Arash Abdi Ghafoori <arash.abdi.gh@gmail.com>
2023-07-21 01:18:15 +0430

    build: change the build process

--- core/rollup.config.js
+++ core/rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import ts from "rollup-plugin-ts";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -7,21 +8,24 @@ export default [
   {
     input: "./src/main.ts",
     output: {
-      dir: "./out/",
+      dir: "./lib/",
       format: "es",
     },
     plugins: [
       nodeResolve(),
-      typescript({
-        module: "es6",
-        allowJs: true,
-        sourceMap: !production,
-        inlineSources: !production,
-        noImplicitAny: true,
-        compilerOptions: {
-          rootDir: ".",
-        },
-      }),
+      // typescript({
+      //   module: "es6",
+      //   allowJs: true,
+      //   sourceMap: !production,
+      //   inlineSources: !production,
+      //   noImplicitAny: true,
+      //   compilerOptions: {
+      //     declaration: true,
+      //     declarationMap: true,
+      //     outDir: "./lib/",
+      //   },
+      // }),
+      ts({ tsconfig: production ? "tsconfig.json" : "tsconfig.json" }),
     ],
   },
 ];
