ec3078a0a5ffa65354b599ee0b2acb5e0f6de3a7
Arash Abdi Ghafoori <arash.abdi.gh@gmail.com>
2023-07-21 01:17:01 +0430

    fix: fix rollup config file

--- core/rollup.config.js
+++ core/rollup.config.js
@@ -5,31 +5,10 @@ const production = !process.env.ROLLUP_WATCH;
 
 export default [
   {
-    input: "./src/core/src/main.ts",
-    output: {
-      dir: "./out/core/",
-      format: "es", 
-    },
-    plugins: [
-      nodeResolve(),
-      typescript({
-        module: "es6",
-        allowJs: true,
-        sourceMap: !production,
-        inlineSources: !production,
-        noImplicitAny: true,
-        compilerOptions: {
-          rootDir: "./src/core",
-          outDir: "./out/core/",
-        },
-      }),
-    ],
-  },
-  {
     input: "./src/main.ts",
     output: {
       dir: "./out/",
-      format: "iife",
+      format: "es",
     },
     plugins: [
       nodeResolve(),
